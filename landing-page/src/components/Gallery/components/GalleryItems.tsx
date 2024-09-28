3609be846bc9023f7a5fc864ddc7b171e6d97d42
mdedealf <dede.alfaruqq@gmail.com>
2024-09-28 13:18:14 +0700

    feat: add gallery works styling when user hover show the button and some effects

--- landing-page/src/components/Gallery/components/GalleryItems.tsx
+++ landing-page/src/components/Gallery/components/GalleryItems.tsx
@@ -1,20 +1,22 @@
 import { FC } from "react";
-import ArrowIcon from "../../../assets/icons/arrow-forward.svg";
 
 const GalleryItems: FC<Gallery> = ({ image, alt, link }) => {
   return (
     <>
-      <div className="relative flex items-center justify-center bg-light-gray h-[306px] w-[444px] rounded-[4px]">
-        <img className="w-[348px] h-[194px]" src={image} alt={alt} />
-        <div className="absolute flex justify-center items-center bg-white py-[12px] px-[16px] rounded-[4px] gap-[8px]">
-          <a className="text-[16px] font-500px" href={link} target="_blank">
-            Visit the website
+      <div className="relative group hover:shadow-2xl flex items-center justify-center bg-light-gray h-[306px] w-[444px] rounded-[4px]">
+        <img
+          className="w-[348px] h-[194px] object-cover blur-none group-hover:blur-sm"
+          src={image}
+          alt={alt}
+        />
+        <div className="absolute flex justify-center items-center bg-white rounded-[4px] gap-[8px] cursor-pointer transition duration-300">
+          <a
+            className="hidden py-[12px] px-[16px] group-hover:block text-[16px] font-500px"
+            href={link}
+            target="_blank"
+          >
+            Visit the website ↗
           </a>
-          <img
-            className="w-[20px] h-[20px]"
-            src={ArrowIcon}
-            alt="arrow forward icon"
-          />
         </div>
       </div>
     </>
