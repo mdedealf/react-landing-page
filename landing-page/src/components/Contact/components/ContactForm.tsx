73a9fce0f6332529c81d6deccc824b8a314801c5
mdedealf <dede.alfaruqq@gmail.com>
2024-10-04 11:48:33 +0700

    fix: fixing some bugs

--- landing-page/src/components/Contact/components/ContactForm.tsx
+++ landing-page/src/components/Contact/components/ContactForm.tsx
@@ -46,7 +46,7 @@ const ContactForm: FC = () => {
     subject: Yup.string().required("Subject is required"),
     message: Yup.string()
       .required("Message is required")
-      .min(200, "You can only input up to 200 characters"),
+      .max(200, "You can only input up to 200 characters"),
   });
 
   return (
